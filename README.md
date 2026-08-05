d853e96e539e99d3d4f967b07876cca0457112b7
Pedro Jorge Neves Lira <pedrojorge.dev@gmail.com>
2026-08-05 10:04:16 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Sistema de Chamados
+
+## Objetivo
+
+Estou desenvolvendo esse projeto com objetivo de aprender na prática conceitos do básico até o avançado da área onde desejo atuar.
+
+## Tecnologias
+
+- HTML
+- CSS
+- JavaScript
+- Módulos ESM
+
+## Funcionalidades concluídas
+
+- Criar, excluir e editar novo chamado.
+- Tratamento de caso quando não há nenhum chamado.
+
+## Aprendizados
+
+- Aprendi como eu deveria pensar na criação de novas funcionalidades.
+- Achei erros, bugs e consertei eu mesmo.
+- Descobri o que é e como usar funções como
+-- map() -> itera sobre cada item de um array, aplica uma função em cada um(pode ser uma verificação ou etc) e retorna um novo array.
+-- arrow function -> uma forma bastante abreviada em js de uma function
+- Aprendi que devemos usar event/params nas arrow functions quando for preciso consultar alguma informacao do event.
+-- Exemplo: o usuario clicou em um botao dentro de um card, então a gente precisa saber qual é o botão que foi acionado (event.target)
+-- Exemplo : o usuario clicou para dar submit no form, entao eu preciso impedir o comportamento padrao de reiniciar a pagina para poder executar -- a minha funcao (event.preventDefault()).
+
+## Próximas etapas
+
+- Pesquisa e filtro dos chamados, ordenacão, persistência com localStorage e JSON, camada assíncrona, Loading e tratamento de erros e a organização mínima em módulos.
+- Depois de abordar os tópicos em JS, partir para o JS Moderno : TypeScript, React, integrar Java e Spring Boot no projeto,PostgresSQL,integração full-stack, testes e deploy.
+- Com isso, vou ter um projeto do inicio ao fim feito na mão por mim, sem ajuda direta de IA, apenas para me guiar durante o processo.
---
+++ src/main.js
@@ -0,0 +1,143 @@
+import { chamados } from "../data/chamados.js";
+
+let chamadosAtuais = [...chamados];
+
+
+const container = document.querySelector("#lista-chamados");
+const msgVazio = document.querySelector("#msg-vazio")
+const botaoNovoChamado = document.querySelector("#btn-novo-chamado");
+const viewFormularioNovoChamado = document.querySelector("#view-form-chamado");
+const viewChamados = document.querySelector("#view-chamados");
+const botaoCancelar = document.querySelector("#btn-cancelar-form");
+const formChamado = document.querySelector("#form-chamado");
+let idEmEdicao = null;
+
+function criarCardChamado(chamado) {
+  const classePrioridade = chamado.prioridade.toLowerCase();
+  const classeStatus = chamado.status.toLowerCase().replace(" ", "-");
+
+  return `<li class="card-chamado" data-id="${chamado.id}">
+    <div class="card-chamado__topo">
+      <h3 class="card-chamado__titulo">${chamado.titulo}</h3>
+      <div class="badges">
+        <span class="badge badge--${classePrioridade}">${chamado.prioridade}</span>
+        <span class="badge badge--${classeStatus}">${chamado.status}</span>
+      </div>
+    </div>
+    <p class="card-chamado__cliente" name="clienteNome">${chamado.clienteNome}</p>
+    <div class="card-chamado__acoes">
+      <button class="btn btn--pequeno btn--fantasma btn-editar">Editar</button>
+      <button class="btn btn--pequeno btn--sucesso btn-concluir">
+        Concluir
+      </button>
+      <button class="btn btn--pequeno btn--perigo btn-excluir">Excluir</button>
+    </div>
+  </li>`;
+}
+
+// Para cada chamado dentro de chamados, colocar esse HTML.
+function renderizarChamados(listaDeChamados) {
+
+  if (listaDeChamados.length !== 0) {
+    container.hidden = false;
+    msgVazio.hidden = true;
+    container.innerHTML = listaDeChamados.map(criarCardChamado).join("");
+  } else {
+    container.innerHTML = "";
+    msgVazio.hidden = false;
+    container.hidden = true;
+  }
+}
+renderizarChamados(chamadosAtuais);
+
+// BOTÃO CRIAR NOVO CHAMADO
+botaoNovoChamado.addEventListener("click", () => {
+  viewChamados.hidden = true;
+  viewFormularioNovoChamado.hidden = false;
+  idEmEdicao = null;
+  formChamado.reset();
+});
+
+// BOTÃO ENVIAR FORM: EDIÇÃO/NOVO
+formChamado.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const dadosForm = Object.fromEntries(new FormData(formChamado));
+  if (idEmEdicao !== null) {
+    chamadosAtuais = chamadosAtuais.map((chamado) => {
+      if (chamado.id === idEmEdicao) {
+        return {
+          ...chamado,
+          ...dadosForm,
+        };
+      }
+
+      return chamado;
+    });
+  } else {
+    let proximoId = 0;
+    if(chamadosAtuais.length !== 0) {
+      proximoId = Math.max(...chamadosAtuais.map((chamado) => chamado.id)) + 1;
+    } else {
+      proximoId = 1;
+    }
+
+    const novoChamado = {
+      ...dadosForm,
+      id: proximoId,
+      dataAbertura: new Date().toISOString().slice(0, 10),
+    };
+
+    chamadosAtuais = [...chamadosAtuais, novoChamado];
+  }
+
+  renderizarChamados(chamadosAtuais);
+  viewChamados.hidden = false;
+  viewFormularioNovoChamado.hidden = true;
+  formChamado.reset();
+  idEmEdicao = null;
+});
+
+// BOTÃO EXCLUIR
+container.addEventListener("click", (event) => {
+  if (!event.target.classList.contains("btn-excluir")) {
+    return;
+  }
+
+  const card = event.target.closest(".card-chamado");
+  const id = Number(card.dataset.id);
+
+  chamadosAtuais = chamadosAtuais.filter((chamado) => chamado.id !== id);
+
+  renderizarChamados(chamadosAtuais);
+});
+
+
+// BOTÃO EDITAR
+container.addEventListener("click", (event) => {
+  if (!event.target.classList.contains("btn-editar")) {
+    return;
+  }
+
+  const card = event.target.closest(".card-chamado");
+  const id = Number(card.dataset.id);
+
+  const chamado = chamadosAtuais.find((chamado) => chamado.id === id);
+  idEmEdicao = chamado.id;
+
+  formChamado.elements.titulo.value = chamado.titulo;
+  formChamado.elements.clienteNome.value = chamado.clienteNome;
+  formChamado.elements.prioridade.value = chamado.prioridade;
+  formChamado.elements.status.value = chamado.status;
+
+  viewChamados.hidden = true;
+  viewFormularioNovoChamado.hidden = false;
+});
+
+
+// BOTÃO CANCELAR
+botaoCancelar.addEventListener("click", () => {
+  idEmEdicao = null;
+  formChamado.reset();
+  viewChamados.hidden = false;
+  viewFormularioNovoChamado.hidden = true;
+})
