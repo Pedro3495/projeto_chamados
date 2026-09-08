01c3c43ca7645c174706cf6d6a56b5027e00235b
Pedro Jorge Neves Lira <pedrojorge.dev@gmail.com>
2026-09-08 11:33:18 -0300

    refactor: migrar filtros para TypeScript

--- src/data/chamados.ts
+++ src/data/chamados.ts
@@ -7,9 +7,9 @@ type Chamado = {
   titulo: string;
 };
 
-type Prioridade = "Baixa" | "Media" | "Alta" | "Urgente";
-type Status = "Aberto" | "Em andamento" | "Aguardando cliente" | "Concluído";
+type Prioridade = "Baixa" | "Media" | "Alta" | "Urgente" | "";
+type Status = "Aberto" | "Em andamento" | "Aguardando cliente" | "Concluído" | "";
 
 const chamados: Chamado[] = [];
 
-export { chamados, Chamado };
+export { chamados, Chamado, Status, Prioridade };
